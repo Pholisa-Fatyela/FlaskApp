f3b5b8381af746854d6bd3e32da85bae56348497
Pholisa-Fatyela <pholisa@projectcodex.co>
2016-02-15 16:21:46 +0200

    Added weather route

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,10 @@
-from flask import Flask, render_template
+import pyowm
+from flask import Flask, request, jsonify, render_template
+
 
 app = Flask(__name__)
+
+owm = pyowm.OWM('f7d3136786b15c2e1a3fb486b5df296a')
 
 @app.route('/')
 def home():
@@ -9,6 +13,14 @@ def home():
 @app.route('/about')
 def about():
     return render_template('about.html')
+
+@app.route('/weather', methods=['GET', 'POST'])
+def weather():
+    longitude = float(request.args.get('long', [18.5003])[0])
+    latitude = float(request.args.get('lat', [-33.871])[0])
+    observation = owm.weather_at_coords(longitude, latitude)
+    results = observation.get_weather()
+    return jsonify(results.get_temperature('celsius'))
 
 if __name__ == '__main__':
   app.run(debug=True)
